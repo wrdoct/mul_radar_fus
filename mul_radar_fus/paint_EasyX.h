009183d51052e9fc2e3ba01ebc092571b92bc13a
wrdoct <llw18729357726@163.com>
2022-02-22 20:25:26 +0800

    Create paint_EasyX.h

---
+++ mul_radar_fus/paint_EasyX.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "tools.h"
+
+#include <graphics.h>		// 引用图形库头文件
+#include <conio.h>
+
+using namespace std;
+using namespace Eigen;     // 改成这样亦可 using Eigen::MatrixXd; 
+
+void radarPaint(vector<Eigen::Vector2d> &radarPoints);
