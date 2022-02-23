1930357f86dd326fd9b75665a919b6c763f82050
wrdoct <llw18729357726@163.com>
2022-02-23 21:09:12 +0800

    Update paint_EasyX.h

--- paint_EasyX.h
+++ paint_EasyX.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "coordinate_transformation.h"
+#include "tools.h"
 
 #include <graphics.h>		// 引用图形库头文件
 #include <conio.h>
@@ -7,4 +7,26 @@
 using namespace std;
 using namespace Eigen;     // 改成这样亦可 using Eigen::MatrixXd; 
 
-void radarPaint(vector<Eigen::Vector2d> &radarPoints);
+//画图 //验证程序使用
+class GRAPH {
+public:
+	GRAPH();
+	~GRAPH();
+
+void radarPaint(vector<RadarInfo_t> &radarFrameInfos);
+
+//显示的参数设置
+void ParaConfig();
+
+//画车道
+void Road();
+
+private:
+	int carNum;
+	IMAGE img;
+
+	int x1;
+	int y1;
+	int road_w;
+	int appear_length;
+};
